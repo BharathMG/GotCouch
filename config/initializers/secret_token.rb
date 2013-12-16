94eaedd96e8da2e7a426f007095359881815a007
BharathMG <mgbharath@ymail.com>
2013-12-16 19:44:36 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GotCouch::Application.config.secret_key_base = 'a11ab668e76c5a98782b768020cdd43aa4fac7c2917c6290b181f7b9c59f122b1aa905ca25d9ab6775db094ea99c170efc21563830ca05aa216545d0abf7ffad'
